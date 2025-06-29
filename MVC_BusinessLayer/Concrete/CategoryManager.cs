fe7a692b25faad60e60b7983b403a420e7d3fee3
MertCanCelebi <83113602+MertCanCelebi@users.noreply.github.com>
2025-06-29 16:56:08 +0300

    Add files via upload
    Katmanların Projeye eklenmesi

---
+++ MVC_BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using MVC_DataAccessLayer.Concrete.Repositories;
+using MVC_EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBl()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBl(Category p)
+        {
+            if(p.CategoryName == "" || p.CategoryName.Length > 50)
+            {
+                //hata mesajı yazılacak
+            }
+            else
+            {
+                repo.Insert(p);
+                
+            }
+        }
+    }
+}
