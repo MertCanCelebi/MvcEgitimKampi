c33d76b70ea1245bc9a9d176dcb6887949a4edf5
MertCanCelebi <83113602+MertCanCelebi@users.noreply.github.com>
2025-07-08 20:38:47 +0300

    72. ders sonu

--- MVC_EntityLayer/Concrete/Message.cs
+++ MVC_EntityLayer/Concrete/Message.cs
@@ -7,18 +7,17 @@ using System.Threading.Tasks;
 
 namespace MVC_EntityLayer.Concrete
 {
-    public class Contact
+    public class Message
     {
         [Key]
-        public int ContactId { get; set; }
+        public int MessageId { get; set; }
         [StringLength(50)]
-        public string UserName { get; set; }
+        public string SenderMail { get; set; }
         [StringLength(50)]
-        public string UserMail { get; set; }
-        [StringLength(50)]
+        public string ReciverMail { get; set; }
+        [StringLength(100)]
         public string Subject { get; set; }
-        [StringLength(1000)]
-        public string Message { get; set; }
-        public DateTime ContactDate { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime MessageDate { get; set; }
     }
 }
