616a0f7c7c19e8b1601c9147fd6741bbf8461e87
MertCanCelebi <83113602+MertCanCelebi@users.noreply.github.com>
2025-07-11 10:45:14 +0300

    100. ders sonu

---
+++ MVC_EntityLayer/Concrete/Talent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_EntityLayer.Concrete
+{
+    public class Talent
+    {
+        [Key]
+        public int TalentID { get; set; }
+        [StringLength(100)]
+        public string TalentName { get; set; }
+        public int TalentLevel { get; set; }
+    }
+}
