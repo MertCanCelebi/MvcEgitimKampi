80fa40ec3ca4dac603db3aa42e76703723492c69
MertCanCelebi <83113602+MertCanCelebi@users.noreply.github.com>
2025-07-11 10:50:45 +0300

    100. ders sonu

---
+++ MVC_DataAccessLayer/Migrations/202507101218134_addtalent.cs
@@ -0,0 +1,27 @@
+﻿namespace MVC_DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addtalent : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Talents",
+                c => new
+                    {
+                        TalentID = c.Int(nullable: false, identity: true),
+                        TalentName = c.String(maxLength: 100),
+                        TalentLevel = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.TalentID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Talents");
+        }
+    }
+}
